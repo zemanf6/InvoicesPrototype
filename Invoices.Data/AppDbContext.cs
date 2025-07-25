319e58db85d3d016abb9de8e1fb1f33f857f66dd
zeman.fi6 <zeman.fi6@gmail.com>
2025-07-25 16:15:09 +0200

    SeedingL

--- Invoices.Data/AppDbContext.cs
+++ Invoices.Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Invoices.Data.Entities;
+using Invoices.Data.Entities.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace Invoices.Data
@@ -17,6 +18,44 @@ namespace Invoices.Data
             // Aby se enum Country v databázi ukládal jako string a ne jako číslo
             modelBuilder.Entity<Person>(builder =>
             {
+                builder.HasData(
+                        new Person
+                        {
+                            Id = 1,
+                            Name = "Jan Novák",
+                            IdentificationNumber = "12345678",
+                            TaxNumber = "CZ12345678",
+                            AccountNumber = "1234567890",
+                            BankCode = "0100",
+                            Iban = "CZ6501000000001234567890",
+                            Telephone = "+420123456789",
+                            Mail = "jan.novak@example.com",
+                            Street = "Ulice 1",
+                            Zip = "10000",
+                            City = "Praha",
+                            Country = Country.CZECHIA,
+                            Note = "Testovací osoba",
+                            Hidden = false
+                        },
+                        new Person
+                        {
+                            Id = 2,
+                            Name = "Anna Horváthová",
+                            IdentificationNumber = "87654321",
+                            TaxNumber = "SK87654321",
+                            AccountNumber = "0987654321",
+                            BankCode = "0900",
+                            Iban = "SK8909000000000987654321",
+                            Telephone = "+421987654321",
+                            Mail = "anna.horvathova@example.sk",
+                            Street = "Cesta 5",
+                            Zip = "81101",
+                            City = "Bratislava",
+                            Country = Country.SLOVAKIA,
+                            Note = "Druhá osoba",
+                            Hidden = false
+                        }
+                    );
                 builder.Property(p => p.Country)
                 .HasConversion<string>();
 
