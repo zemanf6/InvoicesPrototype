e5b5ff92890139952782f0c1eeef1cd3e8efd804
zeman.fi6 <zeman.fi6@gmail.com>
2025-07-25 16:46:20 +0200

    RT seed

--- Invoices.Api/Program.cs
+++ Invoices.Api/Program.cs
@@ -9,6 +9,8 @@ using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using Invoices.Api.Validators;
 using FluentValidation;
+using Invoices.Data.Entities.Enums;
+using Invoices.Data.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
@@ -42,6 +44,53 @@ builder.Services.AddDbContext<AppDbContext>(options =>
            .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (!db.Persons.Any())
+    {
+        db.Persons.AddRange(
+            new Person
+            {
+                Name = "Jan Novák",
+                IdentificationNumber = "12345678",
+                TaxNumber = "CZ12345678",
+                AccountNumber = "1234567890",
+                BankCode = "0100",
+                Iban = "CZ6501000000001234567890",
+                Telephone = "+420123456789",
+                Mail = "jan.novak@example.com",
+                Street = "Ulice 1",
+                Zip = "10000",
+                City = "Praha",
+                Country = Country.CZECHIA,
+                Note = "Testovací osoba",
+                Hidden = false
+            },
+            new Person
+            {
+                Name = "Anna Horváthová",
+                IdentificationNumber = "87654321",
+                TaxNumber = "SK87654321",
+                AccountNumber = "0987654321",
+                BankCode = "0900",
+                Iban = "SK8909000000000987654321",
+                Telephone = "+421987654321",
+                Mail = "anna.horvathova@example.sk",
+                Street = "Cesta 5",
+                Zip = "81101",
+                City = "Bratislava",
+                Country = Country.SLOVAKIA,
+                Note = "Druhá osoba",
+                Hidden = false
+            }
+        );
+
+        db.SaveChanges();
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
